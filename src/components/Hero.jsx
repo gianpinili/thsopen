ffaaa38e57d45fb6ba6a3f530605ce02fa8e7f36
Ian Pinili <gianpinili@gmail.com>
2024-06-26 09:44:10 -0700

    transfer to mobile view - checking on mobile local network

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import heroVid from '../assets/hero.mp4';
 import useIntersectionObserver from '../useIntersectionObserver';
 import { Link } from 'react-router-dom';
@@ -6,21 +7,33 @@ function Hero() {
  const [ref, isIntersecting] = useIntersectionObserver({
   threshold: 0.1,
  });
+ const videoRef = useRef(null);
+
+ useEffect(() => {
+  if (videoRef.current) {
+   videoRef.current.load();
+   videoRef.current.play().catch(error => {
+    // Handle playback error
+    console.error('Playback prevented', error);
+   });
+  }
+ }, []);
 
  return (
   <div ref={ref} className={`mt-[2rem] 2xl:mt-[3.3rem] font-spacemono relative w-full max-h-screen overflow-hidden transition-opacity duration-[2.5s] ${isIntersecting ? 'opacity-100' : 'opacity-0'}`}>
    <video
+    ref={videoRef}
     autoPlay
     loop
     muted
-    className="w-full mt-[-2rem] md:mt-[-2rem] lg:mt-[-2rem] xl:mt-[-2rem] 2xl:mt-[-2.25rem] h-[100vh] md:h-[100vh] lg:h-[100vh] xl:h-[100vh] object-cover opacity-70 pointer-events-none"
+    className="w-full mt-[-2rem] md:mt-[-2rem] lg:mt-[-2rem] xl:mt-[-2rem] 2xl:mt-[-2.25rem] h-[100vh] md:h-[100vh] lg:h-[100vh] xl:h-[100vh] object-cover opacity-70 disable-video-interaction"
    >
     <source src={heroVid} type="video/mp4" />
     Your browser does not support the video tag.
    </video>
    <div className="absolute top-[35%] bg-none z-10 flex flex-col items-center justify-center gap-10 md:mx-[5rem] lg:mx-[10rem] xl:mx-[15rem] 2xl:mx-[20rem]">
     <h1 className="text-white leading-9 scale-y-200 tracking-wide text-[2rem] md:text-[3rem] xl:text-[3.1rem] bg-transparent uppercase font-poppins font-extrabold text-center">Register Your Team</h1>
-    <p className='text-center text-[.8rem] md:text-[1rem] mx-6 lg:text-[1.1rem]'>This isn&apos;t just any golf tournament--it&apos;s an opportunnity to enjoy 18 holes of challenging play while supporting a fantastic cause. All proceeds from the event will go to the Canucks Autism Network (CAN) helping to raise awareness and support for autism.</p>
+    <p className='text-center text-[.8rem] md:text-[1rem] mx-6 lg:text-[1.1rem]'>This isn&apos;t just any golf tournament--it&apos;s an opportunity to enjoy 18 holes of challenging play while supporting a fantastic cause. All proceeds from the event will go to the Canucks Autism Network (CAN) helping to raise awareness and support for autism.</p>
     <Link to={'/contact'} className="border-white border py-2 px-4 rounded mt-4 hover:bg-slate-100 hover:text-black duration-200 md:py-4 md:px-8">Registration Coming Soon</Link>
     {/* https://docs.google.com/forms/d/e/1FAIpQLSdXnnFZA88NFBH-Fg546B_edYD9xTRObjKpZX3C82Igq5Ls4A/viewform?vc=0&c=0&w=1&flr=0 */}
    </div>
