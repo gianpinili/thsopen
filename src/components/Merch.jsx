f344955aeda5c0ac255db2ee4b28a2d993508ecb
Ian Pinili <gianpinili@gmail.com>
2024-07-02 11:52:19 -0700

    update merch section style for prod

--- src/components/Merch.jsx
+++ src/components/Merch.jsx
@@ -24,14 +24,14 @@ function Merch() {
  return (
   <div
    ref={ref}
-   className={`py-12 bg-white text-black px-[1.5rem] md:px-[2rem] lg:px-[3rem] font-spacemono 2xl:px-[8rem] transition-opacity duration-[1s] ${isIntersecting ? 'opacity-100' : 'opacity-0'}`}
+   className={`py-12 bg-black text-white px-[1.5rem] md:px-[2rem] lg:px-[3rem] font-spacemono 2xl:px-[8rem] transition-opacity duration-[1s] ${isIntersecting ? 'opacity-100' : 'opacity-0'}`}
   >
    {/* <h1 className="font-poppins font-semibold text-[2rem] md:text-5xl uppercase tracking-tighter">Shop the merch</h1> */}
    <div className='mt-[1rem] mb-12 flex flex-col-reverse lg:flex-row-reverse md:items-center md:gap-10 md:justify-center'>
     <div className='md:w-1/2 lg:px-[3rem] mt-[3rem]  lg:mt-[-3rem] xl:mt-[-5rem]'>
      <h1 className="font-poppins font-bold text-[4rem] md:text-[6rem] uppercase tracking-tighter">MERCH</h1>
      <p className="my-3 mb-10 md:text-xl">Swing for a cause. All proceeds go to Canucks Autism Network (CAN), helping to raise awareness and support for autism.</p>
-     <a className="border border-black px-6 py-3 rounded-md hover:bg-[#1c1c1c] hover:text-white duration-200" target='_blank' href="https://www.thehockeyshop.com/collections/ths-open">Shop THS Open</a>
+     <a className="border border-white px-6 py-3 rounded-md hover:bg-white hover:text-black hover:font-bold duration-200" target='_blank' href="https://www.thehockeyshop.com/collections/ths-open">Shop THS Open</a>
     </div>
     <div className='grid grid-cols-2 gap-4 m-auto'>
      <img src={hatImage} alt="THS Open Merch" className='min-w-[9rem] md:max-w-[17.5rem] cursor-pointer hover:scale-105 duration-300' loading='lazy' onClick={() => toggleModal(hatImage)} />
