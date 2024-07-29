aa7b7c82527dfeabf559016b7088ae9d4382f7fb
Ian Pinili <gianpinili@gmail.com>
2024-07-29 12:21:22 -0700

    update footer style + add about link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,11 +12,12 @@ function Footer() {
   return (
     <>
       <footer className="bg-white text-black py-5 font-spacemono px-[1.5rem]">
-        <div className='flex flex-col gap-2 mt-12 mb-5 md:flex-row md:justify-evenly'>
+        <div className='flex flex-col gap-2 mt-12 text-lg sm:text-xl mb-5 md:flex-row md:justify-center md:gap-[3.5rem]'>
           <Link to={'/'} onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} className='hover:scale-110 md:hover:scale-125 duration-300 hover:ml-[1.5rem] md:hover:border-b border-black md:text-xl'>Home</Link>
+          <Link to={'/about'} onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} className='hover:scale-110 md:hover:scale-125 hover:ml-[1.5rem] duration-300 md:hover:border-b border-black md:text-xl'>About</Link>
+          <Link to={'/sponsorship'} className='hover:scale-110 md:hover:scale-125 hover:ml-[1.5rem] duration-300 md:hover:border-b border-black md:text-xl'>Sponsor</Link>
           <Link to={'/events'} className='hover:scale-110 md:hover:scale-125 hover:ml-[1.5rem] md:hover:border-b border-black duration-300 md:text-xl'>Events</Link>
           <Link to={'/merch'} className='hover:scale-110 md:hover:scale-125 hover:ml-[1.5rem] duration-300 md:hover:border-b border-black md:text-xl'>Merch</Link>
-          <Link to={'/sponsorship'} className='hover:scale-110 md:hover:scale-125 hover:ml-[1.5rem] duration-300 md:hover:border-b border-black md:text-xl'>Sponsor</Link>
           <Link to={'/contact'} className='hover:scale-110 md:hover:scale-125 hover:ml-[1.5rem] duration-300 md:hover:border-b border-black md:text-xl'>Contact</Link>
         </div>
         <div className='flex flex-col items-center justify-center gap-8 mb-10 md:flex-row md:my-12 md:gap-14'>
