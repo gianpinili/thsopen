e872fb20bf3060642b390fb9c20156902c6f9d99
Ian Pinili <gianpinili@gmail.com>
2024-08-08 16:24:27 -0700

    update sponsor section

--- src/components/Sponsor.jsx
+++ src/components/Sponsor.jsx
@@ -2,11 +2,8 @@ import { useState } from 'react';
 import { ChevronDown, ChevronUp } from 'lucide-react';
 import useIntersectionObserver from '../useIntersectionObserver';
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
 import OurSponsors from './OurSponsors';
-=======
 import { v4 as uuidv4 } from 'uuid';
->>>>>>> 39f36ca0ace9d118662e3b410d10e95e89e1e33e
 
 function Sponsor() {
  const sponsorData = [
